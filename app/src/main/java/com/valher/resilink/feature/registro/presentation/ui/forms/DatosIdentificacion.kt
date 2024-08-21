c68e529e95fd6bc772d86b78edf18a97edac2f25
Edson Valenzuela <administrador@MacBook-Pro-de-administrador.local>
2024-08-21 13:03:26 -0600

    se mejora la muestra de imagenes en los documentos y foto

--- app/src/main/java/com/valher/resilink/feature/registro/presentation/ui/forms/DatosIdentificacion.kt
+++ app/src/main/java/com/valher/resilink/feature/registro/presentation/ui/forms/DatosIdentificacion.kt
@@ -10,7 +10,9 @@ import android.util.Log
 import android.widget.DatePicker
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -21,7 +23,9 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -38,7 +42,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -196,28 +202,41 @@ fun DatosIdentificacion(
             Spacer(modifier = Modifier.height(16.dp))
 
             // Foto personal
-            Box {
-                Row(verticalAlignment = Alignment.CenterVertically) {
+            Box (
+                modifier = Modifier.fillMaxWidth()
+            ){
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
                     if (userPhotoUri.value != null) {
                         val bitmapFotoUsuario = remember(userPhotoUri.value) { getBitmapFromUri(userPhotoUri.value!!, activity) }
                         bitmapFotoUsuario?.let {
                             Image(
                                 bitmap = it.asImageBitmap(),
                                 contentDescription = null,
+                                contentScale = ContentScale.Crop,
                                 modifier = Modifier
-                                    .width(50.dp)
-                                    .height(50.dp)
+                                    .size(50.dp)
+                                    .clip(RoundedCornerShape(8.dp))
+                                    .border(
+                                        BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
+                                        shape = RoundedCornerShape(8.dp))
                                     .clickable { isUserPhotoPreviewVisible.value = true }
                             )
                         }
                     } else {
                         Image(
                             painter = painterResource(id = R.drawable.sinimagen),
+                            contentScale = ContentScale.Crop,
                             contentDescription = "Imagen por defecto",
                             modifier = Modifier
-                                .width(50.dp)
-                                .height(50.dp)
-                                .clickable { isDocumentPhotoPreviewVisible.value = true }
+                                .size(50.dp)
+                                .clip(RoundedCornerShape(8.dp))
+                                .border(
+                                    BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
+                                    shape = RoundedCornerShape(8.dp))
                         )
                     }
                     Spacer(modifier = Modifier.width(8.dp))
@@ -247,17 +266,28 @@ fun DatosIdentificacion(
             Spacer(modifier = Modifier.height(16.dp))
 
             // Foto del documento
-            Box {
-                Row(verticalAlignment = Alignment.CenterVertically){
+            Box (
+                modifier = Modifier.fillMaxWidth()
+            ){
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start,
+                    modifier = Modifier.fillMaxWidth()
+                ){
                     if (documentPhotoUri.value != null) {
                         val bitmapFotoDocumento = remember(documentPhotoUri.value) { getBitmapFromUri(documentPhotoUri.value!!, activity) }
                         bitmapFotoDocumento?.let {
                             Image(
                                 bitmap = it.asImageBitmap(),
                                 contentDescription = null,
+                                contentScale = ContentScale.Crop,
                                 modifier = Modifier
-                                    .width(50.dp)
-                                    .height(50.dp)
+                                    .size(50.dp)
+                                    .clip(RoundedCornerShape(8.dp))
+                                    .border(
+                                        BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
+                                        shape = RoundedCornerShape(8.dp))
+                                    .clickable { isDocumentPhotoPreviewVisible.value = true }
                             )
                         }
                     }
@@ -265,8 +295,14 @@ fun DatosIdentificacion(
                         Image(
                             painter = painterResource(id = R.drawable.sinimagen),
                             contentDescription = "Imagen por defecto",
+                            contentScale = ContentScale.Crop,
                             modifier = Modifier
-                                .width(50.dp)
+                                .size(50.dp)
+                                .clip(RoundedCornerShape(8.dp))
+                                .border(
+                                    BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
+                                    shape = RoundedCornerShape(8.dp)
+                                )
                         )
                     }
                     Spacer(modifier = Modifier.width(8.dp))
